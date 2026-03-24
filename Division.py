f1be9df9071d8e919f12c2eb1b3567e1ee30ea12
verma097-glitch <sv9524176@gmail.com>
2026-03-24 22:12:43 +0530

    Assignmnt-2

---
+++ Division.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num2 != 0:
+    result = num1 / num2
+    print("Division result is:", result)
+else:
+    print("Cannot divide by zero!")
