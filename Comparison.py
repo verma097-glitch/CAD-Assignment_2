d4406ee19b3b49491ca08860635f9578493765dc
verma097-glitch <sv9524176@gmail.com>
2026-03-24 22:18:19 +0530

    Assignment-2

---
+++ Comparison.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Equal:", num1 == num2)
+print("Not Equal:", num1 != num2)
+print("Greater than:", num1 > num2)
+print("Less than:", num1 < num2)
+print("Greater or Equal:", num1 >= num2)
+print("Less or Equal:", num1 <= num2)
